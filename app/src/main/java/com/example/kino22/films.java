6d262a0c8ca7d7b29419fe732a752f1e0be7a958
vapil <susulya00@mail.ru>
2023-11-26 18:50:09 +0300

    связь с сервером 1 часть

---
+++ app/src/main/java/com/example/kino22/films.java
@@ -0,0 +1,24 @@
+package com.example.kino22;
+
+
+/**
+ * Класс заметок
+ */
+public class films {
+
+
+    public String name;
+
+    public String info;
+
+    public String comm;
+
+    public String image;
+    public films(String name, String info,String comm,String image) {
+        this.name = name;
+        this.info = info;
+        this.comm = comm;
+        this.image = image;
+    }
+
+}
